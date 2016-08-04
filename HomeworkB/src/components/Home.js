8ecc26140230283ad4cd64070a26e18f75427426
Claire Liu <wenjing.liu09@foxmail.com>
2016-08-04 10:23:20 +0800

    initial state

---
+++ HomeworkB/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    <div>
+      <h1>Homework B</h1>
+
+      <h2>Get Started</h2>
+    </div>;
+  }
+}
---
+++ HomeworkB/src/components/NotFoundPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class NotFoundPage extends React.Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    <div>
+      <h4>
+        404 Page Not Found
+      </h4>
+    </div>;
+  }
+}
